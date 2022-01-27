b3e04ed326d098ac3c58a47e2340c68cf700a57a
Aimee Truong <aimtruong@ucdavis.edu>
2022-01-26 19:05:20 -0800

    add coding featured topics

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -5,6 +5,19 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+var buttonClickHandler = function(event){
+    var language = event.target.getAttribute("data-language");
+
+    if(language){
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+};
+
 
 var formSubmitHandler = function(event){
     event.preventDefault();
@@ -95,6 +108,24 @@ var displayRepos = function(repos, searchTerm){
     console.log(searchTerm);
 }
 
+
+var getFeaturedRepos = function(language){
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            });
+        }
+        else{
+            alert("Error: GitHub User Not Found");
+        }
+    });
+};
+
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 
 userFormEl.addEventListener("submit", formSubmitHandler);
 
